3f3ec704ea6201b8c430c6400d0360dca5ecbb47
Tornado6896 <tornado6896@gmail.com>
2025-04-09 00:42:25 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,2 @@
+opkg install tar
+curl -s -L https://github.com/Tornado6896/nfqws-updater/blob/main/nfqws.tar --output nfqws.tar && tar -xvf nfqws.tar -C /opt/usr/bin --overwrite > /dev/null && rm nfqws.tar
