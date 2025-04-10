63266d519d551bf30a34efc0abb2a505c645f9aa
Tornado6896 <tornado6896@gmail.com>
2025-04-10 13:05:07 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-opkg install tar
 fileinstlocal=nfqws_install.sh
 fileupdateLocal=nfqws_update.sh
 fileupdate=https://raw.githubusercontent.com/Tornado6896/nfqws-updater/refs/heads/main/nfqws_install.sh
