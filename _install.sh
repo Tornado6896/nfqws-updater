85929f3ca8b5635502772552a8eaf3c761c1cb4f
Tornado6896 <tornado6896@gmail.com>
2025-04-10 12:25:37 +0300

    Update _install.sh

--- _install.sh
+++ _install.sh
@@ -3,8 +3,7 @@
 fileupdate=https://github.com/Tornado6896/nfqws-updater/blob/main/nfqws_update.sh
 fileinst=https://github.com/Tornado6896/nfqws-updater/blob/main/nfqws_install.sh
 fileDir="/opt/usr/bin/"
-
- curl -O "$fileupdate"  -P "$fileDir" 
- curl -O "$fileinst"  -P "$fileDir" 
- chmod +X /"$fileDir"/"$fileinst"
-  chmod +X /"$fileDir"/"$fileupdate"
+curl -O "$fileupdate"  -P "$fileDir" 
+curl -O "$fileinst"  -P "$fileDir" 
+chmod +X "$fileDir"&&"$fileinst"
+ chmod +X "$fileDir"&&"$fileupdate"
