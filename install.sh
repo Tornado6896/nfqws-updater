2b33ba86594b7307c2647e8b8998d2b7dc2b6b32
Tornado6896 <tornado6896@gmail.com>
2025-04-10 13:29:44 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,9 @@ fileupdateLocal=nfqws_update.sh
 fileupdate=https://raw.githubusercontent.com/Tornado6896/nfqws-updater/refs/heads/main/nfqws_update.sh
 fileinst=https://raw.githubusercontent.com/Tornado6896/nfqws-updater/refs/heads/main/install.sh
 fileDir="/opt/usr/bin/"
-
 wget "$fileupdate" -P "$fileDir"
 wget "$fileinst" -P "$fileDir"
 chmod +x "$fileDir""$fileinstlocal"
 chmod +x "$fileDir""$fileupdateLocal"
-
 nfqws_install.sh
  
