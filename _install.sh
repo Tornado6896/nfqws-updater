8697353947e2ad7cc0926df123046ddc6d9ea400
Tornado6896 <tornado6896@gmail.com>
2025-04-10 13:29:05 +0300

    Delete _install.sh

--- _install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-fileupdate=https://github.com/Tornado6896/nfqws-updater/blob/main/nfqws_update.sh
-fileinst=https://github.com/Tornado6896/nfqws-updater/blob/main/nfqws_install.sh
-fileDir="/opt/usr/bin/"
-curl -O "$fileupdate"  -P "$fileDir" 
-curl -O "$fileinst"  -P "$fileDir" 
-chmod +X "$fileDir"&&"$fileinst"
- chmod +X "$fileDir"&&"$fileupdate"
