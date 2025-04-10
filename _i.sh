7d5e76eb8f99a6ac069e1ed23734466a6624d2ba
Tornado6896 <tornado6896@gmail.com>
2025-04-10 14:40:06 +0300

    Update _i.sh

--- _i.sh
+++ _i.sh
@@ -1,3 +1,3 @@
 opkg install tar
-curl -s -L https://github.com/Tornado6896/nfqws-updater/raw/refs/heads/main/updater.tar --output updater.tar && tar -xvf updater.tar -C /opt/usr/bin --overwrite > /dev/null && rm updater.tar
+curl -s -L https://github.com/Tornado6896/nfqws-updater/raw/refs/heads/main/Updater2.tar --output Updater2.tar && tar -xvf Updater2.tar -C /opt/usr/bin --overwrite > /dev/null && rm Updater2.tar
 nfqws_install.sh
