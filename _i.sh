00bc325b571372ea04fa629679001f9ed0986cad
Tornado6896 <tornado6896@gmail.com>
2025-04-23 11:37:20 +0300

    Update _i.sh

--- _i.sh
+++ _i.sh
@@ -1,4 +1,5 @@
-chmod +x "$0"
+
 opkg install tar
 curl -s -L https://github.com/Tornado6896/nfqws-updater/raw/refs/heads/main/nfqws-updater.tar --output nfqws-updater.tar && tar -xvf nfqws-updater.tar -C /opt/usr/bin --overwrite > /dev/null && rm nfqws-updater.tar
+chmod +x "$0"
 nfqws_install
