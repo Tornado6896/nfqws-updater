8c07fd4ecd9a8d778685bcf170755f39ec21039c
Tornado6896 <tornado6896@gmail.com>
2025-04-23 17:09:16 +0300

    Update _i.sh

--- _i.sh
+++ _i.sh
@@ -2,4 +2,4 @@ cd /opt/sbin
 curl -ss -O https://raw.githubusercontent.com/Tornado6896/nfqws-updater/refs/heads/main/nfqws_updater
 chmod +x "nfqws_updater"
 cd
-nfqws_updater -install-nfqws
+nfqws_updater -menu
